ef2ffbd79f3d76e31a7db7b687a0794116f212ba
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-04-02 12:19:55 +0530

    smtp

--- jpura_dev/home/signals.py
+++ jpura_dev/home/signals.py
@@ -2,7 +2,9 @@ from django.db.models.signals import post_save, post_delete
 from . models import Profile
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.mail import send_mail
 
+from django.conf import settings
 
 
 @receiver(post_save, sender = User)
@@ -12,6 +14,24 @@ def createProfile(sender, instance, created, *args, **kwargs):
         Profile.objects.create(
             user = created_user
         )
+
+    username = instance.username
+
+    subject = f"Welcome {username}"
+    message = 'Thanks for contacting us! We\'ll get back in you soon!'
+    sender = settings.EMAIL_HOST_USER
+    recipient_mail = instance.email
+
+    send_mail(
+        subject,
+        message,
+        sender,
+        [recipient_mail],
+        fail_silently = False,
+    )
+
+
+
 
 @receiver(post_delete, sender = Profile)
 def deleteUser(sender, instance, *args, **kwargs):
--- jpura_dev/jpura_dev/settings.py
+++ jpura_dev/jpura_dev/settings.py
@@ -135,6 +135,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = 'login'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #libry we sue
+EMAIL_HOST = 'smtp.gmail.com' #service provider
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'duvindu.inbox@gmail.com'
+EMAIL_HOST_PASSWORD = 'hhkf pnqn tgdz ylyh'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
