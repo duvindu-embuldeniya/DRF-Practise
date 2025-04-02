411bee06a2f7fa01bf512cbba037b3d4f74dc335
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-04-02 20:05:01 +0530

    +pswd reset

--- jpura_dev/jpura_dev/urls.py
+++ jpura_dev/jpura_dev/urls.py
@@ -19,14 +19,22 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
+
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,
                                    document_root = settings.MEDIA_ROOT)
 
-# if settings.DEBUG == False:
-#     urlpatterns = urlpatterns + static(settings.STATIC_URL,
-#                                    document_root = settings.STATIC_ROOT)
